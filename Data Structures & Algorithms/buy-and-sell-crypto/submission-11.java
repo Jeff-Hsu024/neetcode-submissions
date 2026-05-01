896a75b47ed0718cd8d3479ba2a2b9cf1aa77bab
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-01 22:36:12 +0800

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int bpos = 0;
+        int spos = 1;
+        
+        while (spos < prices.length) {
+            var buy = prices[bpos];
+            var sell = prices[spos];
+
+            var profit = sell - buy;
+            max = Math.max(profit, max);
+
+            if (buy > sell) {
+                bpos = spos;
+            }
+
+            spos = spos + 1;
+        }
+
+        return max;
+    }
+}
