f6876804a4203991f166841d84eedb4e96e1f8f3
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-01 17:31:18 +0800

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        return helper(s).equals(helper(t));
+    }
+
+    private String helper(String s) {
+        var charArray = s.toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+}
