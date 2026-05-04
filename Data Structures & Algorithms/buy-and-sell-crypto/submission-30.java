bb4c45d4b70dd9d5371c05065b2e2d0654c3023b
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-04 22:20:52 +0800

    Add: buy-and-sell-crypto - submission-30

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-30.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+
+        int b = 0;
+        for (int s = 1; s < prices.length; s++) {
+            var buy = prices[b];
+            var sell = prices[s];
+            var profit = sell - buy;
+            res = Math.max(res, profit);
+
+            if (buy > sell) {
+                b = s;
+            }
+        }
+
+        return res;
+    }
+}
