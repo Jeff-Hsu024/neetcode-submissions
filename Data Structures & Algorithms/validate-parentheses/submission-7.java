ae4db8cb449eac60fb79f2ac2762f23b5722dc39
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-04 12:42:58 +0800

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> pairs = Map.of(')', '(', '}', '{', ']', '[');
+        var chars = s.toCharArray();
+        var len = chars.length;
+
+        if (len % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (var c: chars) {
+            if (pairs.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != pairs.get(c)) {
+                    return false;
+                }
+            } else {
+            stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
