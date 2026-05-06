1f9342a7184bae177df902b8b33a0f5a8bf291fe
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-06 11:08:48 +0800

    Add: is-palindrome - submission-47

---
+++ Data Structures & Algorithms/is-palindrome/submission-47.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        var chars = s.toCharArray();
+        int l = 0;
+        int r = chars.length - 1;
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(chars[l])) {
+                l++;
+            }
+            while (l < r && !Character.isLetterOrDigit(chars[r])) {
+                r--;
+            }
+            if (Character.toLowerCase(chars[l]) != Character.toLowerCase(chars[r])) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
