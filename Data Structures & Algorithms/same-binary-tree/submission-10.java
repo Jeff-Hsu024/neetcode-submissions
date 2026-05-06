dacb3c8c462b6ef11fbb3dd2e1bfd69b61ae5122
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-06 15:31:02 +0800

    Add: same-binary-tree - submission-10

---
+++ Data Structures & Algorithms/same-binary-tree/submission-10.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<Pair<TreeNode, TreeNode>> s = new Stack<>();
+        s.push(new Pair<>(p, q));
+        while (!s.isEmpty()) {
+            var last = s.pop();
+            var a = last.getKey();
+            var b = last.getValue();
+            if (a == null && b == null) {
+                continue;
+            }
+            if (a == null || b == null || a.val != b.val) {
+                return false;
+            }
+            s.push(new Pair<>(a.left, b.left));
+            s.push(new Pair<>(a.right, b.right));
+        }
+
+        return true;
+    }
+}
