f6d9453796f52b30042a487ef62ab40a51ade2f7
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-06 20:56:50 +0800

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        var first = 1;
+        var second = 2;
+        for (int i = 3; i <= n; i++) {
+            var result = first +second;
+            first = second;
+            second = result;
+        }
+
+        return second;
+
+    }
+}
