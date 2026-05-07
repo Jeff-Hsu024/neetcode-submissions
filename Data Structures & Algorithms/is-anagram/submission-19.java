536b108fae62d9837f4ee2a5bc0a138268feaa82
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-07 08:16:30 +0800

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        return helper(s).equals(helper(t));
+    }
+
+    private Map<Character, Integer> helper(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (var c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        return map;
+    }
+}
