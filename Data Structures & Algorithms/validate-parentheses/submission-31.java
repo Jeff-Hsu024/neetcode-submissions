221bb477d72dec8e7b9d7d4c9cf45c12d3dc81f3
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-08 13:36:47 +0800

    Add: validate-parentheses - submission-31

---
+++ Data Structures & Algorithms/validate-parentheses/submission-31.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> pair = Map.of('}', '{', ']', '[', ')', '(');
+        Stack<Character> stack = new Stack<>();
+        for (var c : s.toCharArray()) {
+            if (pair.containsKey(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                var last = stack.pop();
+                var expect = pair.get(c);
+                if (last != expect) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
