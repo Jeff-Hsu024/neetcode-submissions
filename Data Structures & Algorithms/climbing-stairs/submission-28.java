dee2fde50d3ab6243d1b99527c1546faa696a047
Jeff-Hsu024 <hsu024722@gmail.com>
2026-05-08 09:14:54 +0800

    Add: climbing-stairs - submission-28

---
+++ Data Structures & Algorithms/climbing-stairs/submission-28.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] stairs = new int[] {1, 2};
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            var reach = i;
+            for (var stair : stairs) {
+                var pre = reach - stair;
+                if (pre >=0 ) {
+                    dp[reach] = dp[reach] + dp[pre];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
