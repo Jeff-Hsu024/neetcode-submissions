884e34c3c2c43f9ebd6e6717dfd947341fa1d8e7
Jeff-Hsu024 <hsu024722@gmail.com>
2026-09-07 11:55:12 +0800

    Add: anagram-groups - submission-33

---
+++ Data Structures & Algorithms/anagram-groups/submission-33.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (var s:strs) {
+            var key = helper(s);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        return map.values().stream().toList();
+    }
+
+    String helper(String s) {
+        var chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+}
